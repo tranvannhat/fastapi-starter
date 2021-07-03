ee8c088e53328c078dc43714a071c250842ed4fc
nhattv <nhat.tv16@gmail.com>
2021-07-03 19:16:32 +0700

    first commit

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,4 @@
-from .item import Item
-from .user import User
+# Import all the models, so that Base has them before being
+# imported by Alembic
+from app.models.base import Base  # noqa
+from app.models.user import User  # noqa
--- app/models/base.py
+++ app/models/base.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
@@ -6,10 +9,20 @@ def to_underscore(name):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-
 @as_declarative()
-class BaseModel:
+class Base:
+    __abstract__ = True
+    __name__: str
+
     # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(self) -> str:
         return to_underscore(self.__name__)
+
+
+class BareBaseModel(Base):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
