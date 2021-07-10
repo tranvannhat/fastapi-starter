1a4a89bc619ad8f841da47593f8174f77b35e709
nhattv <nhat.tv16@gmail.com>
2021-07-10 08:42:36 +0700

    first commit

--- app/api/v1/endpoints/authen.py
+++ app/api/v1/endpoints/authen.py
@@ -29,18 +29,14 @@ def login_access_token(
     """
     OAuth2 compatible token login, get an access token for future requests
     """
-    user = crud.user.authenticate(
-        db, email=form_data.username, password=form_data.password
-    )
+    user = crud.user.authenticate(db, email=form_data.username, password=form_data.password)
     if not user:
         raise CustomException(http_code=status.HTTP_400_BAD_REQUEST, message="Incorrect email or password")
     elif not crud.user.is_active(user):
         raise CustomException(http_code=status.HTTP_400_BAD_REQUEST, message="Inactive user")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
-        "access_token": security.create_access_token(
-            user.id, expires_delta=access_token_expires
-        ),
+        "access_token": security.create_access_token(user.id, expires_delta=access_token_expires),
         "token_type": "bearer",
     }
 
